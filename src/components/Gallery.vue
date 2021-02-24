f870125438d0fae6017d78d84b3faa33a50ce0c4
ippee <ippee.music@gmail.com>
2021-02-24 17:26:26 +0900

    remove lines which are no longer used

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,47 +1,20 @@
 <template>
   <div class="gallery" id="gallery">
-        <images v-if="selector === 'images'"></images>
+    <images></images>
     <navbar></navbar>
   </div>
 </template>
 
 <script>
-import SaveData from '@/mixins/SaveData'
-import AudioFunc from '@/mixins/AudioFunc'
 import Navbar from './Navbar'
 import Images from './Gallery/Images'
 
 export default {
   name: 'gallery',
-  computed: {
-    SaveData() {
-      return SaveData
-    },
-    AudioFunc() {
-      return AudioFunc
-    },
-  },
   components: { Navbar, Images },
-  data() {
-    return {
-      selector: 'images',
-    }
-  },
   mounted: function() {
     document.getElementById('gallery').classList.add('fadein')
   },
-  methods: {
-    updateSelectorValue: function() {
-      var elm = document.getElementsByName('selector')
-      
-      for (let i = 0; i < elm.length; i++) {
-        if (elm[i].checked) {
-          AudioFunc.methods.playSE('se-turn-page1', SaveData.methods.getSEVol())
-          this.selector = elm[i].value
-        }
-      }
-    }
-  }
 }
 </script>
 
