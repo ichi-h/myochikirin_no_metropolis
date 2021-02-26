222885920ff254b76377f50d3a2a9233826039fa
ippee <ippee.music@gmail.com>
2021-02-26 20:01:25 +0900

    update bgm loop function for mac

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -16,6 +16,13 @@ fn main() {
 
   {
     let bgm_sink_ = bgm_sink.clone();
+    tauri::spawn(move || {
+      audio::set_bgm(&bgm_sink_);
+    })
+  }
+
+  {
+    let bgm_sink_ = bgm_sink.clone();
     let se_sink_ = se_sink.clone();
 
     let bell_vec = include_bytes!("../audio/bell.ogg").to_vec();
@@ -28,49 +35,42 @@ fn main() {
       turn_page2: Cursor::new(turn_page2_vec),
     };
 
-    let _ = std::thread::spawn(move || {
-      tauri::AppBuilder::new()
-        .invoke_handler(move |_webview, arg| {
-          use cmd::Cmd::*;
-          match serde_json::from_str(arg) {
-            Err(e) => {
-              Err(e.to_string())
-            }
-            Ok(command) => {
-              match command {
-                // definitions for your custom commands from Cmd here
-                MyCustomCommand { arg } => {
-                  //  your command code
-                  println!("{}", arg);
-                },
-                PlayBGM { volume } => {
-                  let volume_float: f32 = volume.parse::<f32>().unwrap();
-                  audio::play_bgm(&bgm_sink_, volume_float);
-                },
-                ChangeBGMVolme { volume } => {
-                  let volume_float: f32 = volume.parse::<f32>().unwrap();
-                  bgm_sink_.set_volume(volume_float);
-                },
-                PlaySE { file_name, volume } => {
-                  let sound_effect_ = sound_effect.clone();
-
-                  let mut se_sink_lock = se_sink_.lock().unwrap();
-
-                  let volume_float: f32 = volume.parse::<f32>().unwrap();
-                  sound_effect_.play_se(&mut se_sink_lock, &handle, file_name, volume_float);
-                },
-              }
-              Ok(())
-            }
+    tauri::AppBuilder::new()
+      .invoke_handler(move |_webview, arg| {
+        use cmd::Cmd::*;
+        match serde_json::from_str(arg) {
+          Err(e) => {
+            Err(e.to_string())
           }
-        })
-        .build()
-        .run();
-      
-      std::process::exit(0);
-    });
-  }
+          Ok(command) => {
+            match command {
+              // definitions for your custom commands from Cmd here
+              MyCustomCommand { arg } => {
+                //  your command code
+                println!("{}", arg);
+              },
+              PlayBGM { volume } => {
+                let volume_float: f32 = volume.parse::<f32>().unwrap();
+                audio::play_bgm(&bgm_sink_, volume_float);
+              },
+              ChangeBGMVolme { volume } => {
+                let volume_float: f32 = volume.parse::<f32>().unwrap();
+                bgm_sink_.set_volume(volume_float);
+              },
+              PlaySE { file_name, volume } => {
+                let sound_effect_ = sound_effect.clone();
 
-  let bgm_sink_ = bgm_sink.clone();
-  audio::set_bgm(&bgm_sink_);
+                let mut se_sink_lock = se_sink_.lock().unwrap();
+
+                let volume_float: f32 = volume.parse::<f32>().unwrap();
+                sound_effect_.play_se(&mut se_sink_lock, &handle, file_name, volume_float);
+              },
+            }
+            Ok(())
+          }
+        }
+      })
+      .build()
+      .run();
+  }
 }
